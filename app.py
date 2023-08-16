8ff726c32d6add028cbdf6300a8989f72a7bcf85
AbhroChowdhury48HD <achowdhury@48hourdiscovery.com>
2023-08-16 16:23:18 -0600

    adding new pages + flask routes

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/redditGPT')
+def redditGPT():
+    return render_template('redditGPT.html')
+
+@app.route('/youtubeGPT')
+def youtubeGPT():
+    return render_template('youtubeGPT.html')
+
+@app.route('/wikipediaGPT')
+def wikipediaGPT():
+    return render_template('wikipediaGPT.html')
+
+@app.route('/csvGPT')
+def csvGPT():
+    return render_template('csvGPT.html')
+
+@app.route('/documentGPT')
+def documentGPT():
+    return render_template('documentGPT.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
